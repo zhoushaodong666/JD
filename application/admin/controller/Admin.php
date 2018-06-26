85d4e3b0fb3c87a688ebb6d80a0a5515f4d6c4d1
zhoushaodong <1138894663@qq.com>
2018-06-26 22:48:51 +0800

    添加管理员的实现

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -12,22 +12,36 @@ use think\Controller;
 class Admin extends Controller
 {
 
-    //后台管理员登录
+    //后台管理员登录页面
     public function login()
     {
         return $this->fetch();
     }
 
-    //后台管理员列表
+    //后台管理员列表页面
     public function adminlist()
     {
         return $this->fetch();
     }
 
-    //后台管理员添加
-    public function addadmin()
+    //后台管理员添加页面
+    public function add()
     {
         return $this->fetch();
+    }
+
+    //后台管理员添加，提交处理
+    public function addhanddle()
+    {
+        $post = request()->post();
+        var_dump($post);
+        $post['admin_password'] = md5($post['admin_password']);
+        $admin_add_result = db('admin')->insert($post);
+        if ($admin_add_result){
+            $this->success('管理员添加成功');
+        }else{
+            $this->success('管理员添加失败');
+        }
     }
 
 
